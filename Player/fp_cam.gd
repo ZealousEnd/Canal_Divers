3361c52fedd21166a71b713706aba92ebcf0f168
Lorelei A <lmalcaza@uno.edu>
2026-05-26 01:07:22 -0500

    first person walking

---
+++ Player/fp_cam.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+var sensitivity = 0.2
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func _input(event: InputEvent) -> void:
+
+	if event is InputEventMouseMotion:
+		#Handles left and right camera movement
+		get_parent().rotate_y(deg_to_rad(-event.relative.x * sensitivity))
+
+		
+		#Handles up and down camera movement
+		rotate_x(deg_to_rad(-event.relative.y * sensitivity))
+		
+		rotation.x = clamp(rotation.x, deg_to_rad(-90), deg_to_rad(90))
